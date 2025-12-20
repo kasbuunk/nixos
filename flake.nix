86e17a955f4d0273d3de78fa20eeae7b64e67cf2
Kas Buunk <kasbuunk@icloud.com>
2025-12-20 12:18:26 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "NixOS Home Lab";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
